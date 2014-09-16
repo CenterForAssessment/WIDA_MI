ebf1fef340793e58b0ec9323619f84ec5d6c761e
dbetebenner <dbetebenner@gmail.com>
2014-09-16 15:38:43 -0400

    Adding gitignore and 2014 scripts

--- WIDA_MI_SGP.R
+++ WIDA_MI_SGP.R
@@ -11,9 +11,22 @@ require(SGP)
 
 ### Load Data
 
-load("Data/WIDA_MI_Data_LONG_2014.Rdata")
+load("Data/WIDA_MI_Data_LONG.Rdata")
 
 
 ### Run analyses
 
-WIDA_MI_SGP <- abcSGP(WIDA_MI_Data_LONG_2014
+WIDA_MI_SGP <- abcSGP(
+		WIDA_MI_Data_LONG,
+		steps=c("prepareSGP", "analyzeSGP", "combineSGP", "outputSGP"),
+		sgp.percentiles=TRUE,
+		sgp.projections=FALSE,
+		sgp.projections.lagged=FALSE,
+		sgp.percentiles.baseline=FALSE,
+		sgp.projections.baseline=FALSE,
+		sgp.projections.lagged.baseline=FALSE)
+
+
+### Save results
+
+save(WIDA_MI_SGP, file="Data/WIDA_MI_SGP.Rdata")
