4f3c3b371529b5198779d27360ff6174f16dd8d8
dbetebenner <dbetebenner@gmail.com>
2015-03-19 12:29:44 -0400

    Close to final update for SGP 2014 script

--- WIDA_MI_SGP_2014.R
+++ WIDA_MI_SGP_2014.R
@@ -7,6 +7,7 @@
 ### Load SGP package
 
 require(SGP)
+debug(analyzeSGP)
 
 
 ### Load Data
@@ -20,11 +21,11 @@ load("Data/WIDA_MI_Data_LONG_2014.Rdata")
 READING_2014.config <- list(
         READING.2014 = list(
                 sgp.content.areas=rep('READING', 3),
-                sgp.projection.content.areas='READING',
+                sgp.projection.baseline.content.areas='READING',
                 sgp.panel.years=c('2012', '2013', '2014'),
-                sgp.projection.panel.years='2014',
+                sgp.projection.baseline.panel.years='2014',
                 sgp.grade.sequences=list(c('0', '1'), c('0', '1', '2'), c('1', '2', '3'), c('2', '3', '4'), c('3', '4', '5'), c('4', '5', '6'), c('5', '6', '7'), c('6', '7', '8'), c('7', '8', '9'), c('8', '9', '10'), c('9', '10', '11'), c('10', '11', '12')),
-                sgp.projection.grade.sequences=list('0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11')))
+                sgp.projection.baseline.grade.sequences=list('0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11')))
 
 
 ### Run analyses
@@ -34,8 +35,8 @@ WIDA_MI_SGP <- updateSGP(
 		WIDA_MI_Data_LONG_2014,
 		steps=c("prepareSGP", "analyzeSGP", "combineSGP", "outputSGP", "visualizeSGP"),
 		sgp.percentiles=TRUE,
-		sgp.projections=FALSE,
-		sgp.projections.lagged=FALSE,
+		sgp.projections=TRUE,
+		sgp.projections.lagged=TRUE,
 		sgp.percentiles.baseline=FALSE,
 		sgp.projections.baseline=TRUE,
 		sgp.projections.lagged.baseline=FALSE,
@@ -45,4 +46,4 @@ WIDA_MI_SGP <- updateSGP(
 
 ### Save results
 
-save(WIDA_MI_SGP, file="Data/WIDA_MI_SGP.Rdata")
+#save(WIDA_MI_SGP, file="Data/WIDA_MI_SGP.Rdata")
