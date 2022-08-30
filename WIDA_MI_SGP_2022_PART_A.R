dce1c8ed32c0f43ac562b16989e98fe435171a85
Damian Betebenner <dbetebenner@nciea.org>
2022-08-30 01:19:26 -0400

    Update of 2022 straight projections

---
+++ WIDA_MI_SGP_2022_PART_A.R
@@ -0,0 +1,81 @@
+########################################################################################
+###                                                                                  ###
+###                WIDA_MI COVID Skip-2-year & Consecutive-year                      ###
+###                SGP analyses for 2021-2022                                        ###
+###                                                                                  ###
+########################################################################################
+
+###   Load packages
+require(SGP)
+require(SGPmatrices)
+
+###   Load data
+load("Data/WIDA_MI_SGP.Rdata")
+load("Data/WIDA_MI_Data_LONG_2022.Rdata")
+
+###   Add Baseline matrices to SGPstateData
+SGPstateData <- addBaselineMatrices("WIDA_MI", "2020")
+
+###   Read in SGP Configuration Scripts and Combine
+source("SGP_CONFIG/2022/PART_A/READING.R")
+
+WIDA_MI_2022_Consecutive_Year_Config <- READING.2022.config
+WIDA_MI_2022_SKIP_2_YEAR_Config <- READING.2022_SKIP_2_YEAR.config
+
+### Parameters
+parallel.config <- list(BACKEND="PARALLEL", WORKERS=list(PERCENTILES=4, BASELINE_PERCENTILES=4, PROJECTIONS=4, LAGGED_PROJECTIONS=4, SGP_SCALE_SCORE_TARGETS=4))
+
+#####
+###   Run BASELINE SGP analysis for SKIP_2_YEAR (two-year skip) 
+###   create new WIDA_MI_SGP object with historical data
+#####
+
+WIDA_MI_SGP <- updateSGP(
+        what_sgp_object = WIDA_MI_SGP,
+        with_sgp_data_LONG = WIDA_MI_Data_LONG_2022,
+        steps = c("prepareSGP", "analyzeSGP", "combineSGP"),
+        sgp.config = WIDA_MI_2022_SKIP_2_YEAR_Config,
+        sgp.percentiles = TRUE,
+        sgp.projections = FALSE,
+        sgp.projections.lagged = FALSE,
+        sgp.percentiles.baseline = TRUE,  # Skip 2 year SGPs for 2022 comparisons
+        sgp.projections.baseline = FALSE, # Calculated in next step
+        sgp.projections.lagged.baseline = FALSE, # Calculated in last step
+        save.intermediate.results = FALSE,
+        parallel.config = parallel.config
+)
+
+### Rename variables and data.tables appending SKIP_2_YEAR to names
+from.variable.names.sgp <- c("SGP", "SGP_ORDER_1", "SGP_ORDER_1_STANDARD_ERROR", "SGP_ORDER_2", "SGP_ORDER_2_STANDARD_ERROR", "SGP_LEVEL", "SGP_NORM_GROUP", "SGP_STANDARD_ERROR")
+to.variable.names.sgp <- paste(c("SGP", "SGP_ORDER_1", "SGP_ORDER_1_STANDARD_ERROR", "SGP_ORDER_2", "SGP_ORDER_2_STANDARD_ERROR", "SGP_LEVEL", "SGP_NORM_GROUP", "SGP_STANDARD_ERROR"), "SKIP_2_YEAR", sep="_")
+from.variable.names.sgp.baseline <- c("SGP_BASELINE", "SGP_BASELINE_ORDER_1", "SGP_BASELINE_ORDER_1_STANDARD_ERROR", "SGP_BASELINE_ORDER_2", "SGP_BASELINE_ORDER_2_STANDARD_ERROR", "SGP_LEVEL_BASELINE", "SGP_NORM_GROUP_BASELINE", "SGP_BASELINE_STANDARD_ERROR")
+to.variable.names.sgp.baseline <- paste(c("SGP_BASELINE", "SGP_BASELINE_ORDER_1", "SGP_BASELINE_ORDER_1_STANDARD_ERROR", "SGP_BASELINE_ORDER_2", "SGP_BASELINE_ORDER_2_STANDARD_ERROR", "SGP_LEVEL_BASELINE", "SGP_NORM_GROUP_BASELINE", "SGP_BASELINE_STANDARD_ERROR"), "SKIP_2_YEAR", sep="_")
+WIDA_MI_SGP@Data[YEAR=="2022", (to.variable.names.sgp):=.SD, .SDcols=from.variable.names.sgp]
+WIDA_MI_SGP@Data[YEAR=="2022", (to.variable.names.sgp.baseline):=.SD, .SDcols=from.variable.names.sgp.baseline]
+
+sgps.2022.baseline <- grep(".2022.BASELINE", names(WIDA_MI_SGP@SGP[["SGPercentiles"]]))
+sgps.2022 <- setdiff(grep(".2022", names(WIDA_MI_SGP@SGP[["SGPercentiles"]])), sgps.2022.baseline) 
+names(WIDA_MI_SGP@SGP[["SGPercentiles"]])[sgps.2022.baseline] <- gsub(".2022.BASELINE", ".2022.BASELINE.SKIP_2_YEAR", names(WIDA_MI_SGP@SGP[["SGPercentiles"]])[sgps.2022.baseline])
+names(WIDA_MI_SGP@SGP[["SGPercentiles"]])[sgps.2022] <- gsub(".2022", ".2022.SKIP_2_YEAR", names(WIDA_MI_SGP@SGP[["SGPercentiles"]])[sgps.2022])
+
+#####
+###   Run BASELINE SGP analysis for consecutive year
+###   create new WIDA_MI_SGP object with historical data
+#####
+
+WIDA_MI_SGP <- abcSGP(
+        sgp_object = WIDA_MI_SGP,
+        steps = c("prepareSGP", "analyzeSGP", "combineSGP", "outputSGP"),
+        sgp.config = WIDA_MI_2022_Consecutive_Year_Config,
+        sgp.percentiles = TRUE,
+        sgp.projections = FALSE,
+        sgp.projections.lagged = FALSE,
+        sgp.percentiles.baseline = TRUE,  #  Consecutive year SGPs for 2021 comparisons
+        sgp.projections.baseline = FALSE, # 
+        sgp.projections.lagged.baseline = FALSE,
+        save.intermediate.results = FALSE,
+        parallel.config = parallel.config
+)
+
+###   Save results
+save(WIDA_MI_SGP, file="Data/WIDA_MI_SGP.Rdata")
---
+++ WIDA_MI_SGP_2022_PART_B.R
@@ -0,0 +1,51 @@
+###########################################################################
+###                                                                     ###
+###     SGP STRAIGHT projections for skip year SGP analyses for 2022    ###
+###                                                                     ###
+###########################################################################
+
+###   Load packages
+require(SGP)
+require(SGPmatrices)
+require(data.table)
+
+###   Load data
+load("Data/WIDA_MI_SGP.Rdata")
+
+###   Load configurations
+source("SGP_CONFIG/2022/PART_B/READING.R")
+
+WIDA_MI_2022.config <- READING_2022.config
+
+### Parameters
+parallel.config <- list(BACKEND="PARALLEL", WORKERS=list(PERCENTILES=8, BASELINE_PERCENTILES=8, PROJECTIONS=8, LAGGED_PROJECTIONS=8, SGP_SCALE_SCORE_TARGETS=8))
+
+###   Add Baseline matrices to SGPstateData
+SGPstateData <- addBaselineMatrices("WIDA_MI", "2020")
+
+#  Establish required meta-data for STRAIGHT projection sequences
+SGPstateData[["WIDA_MI"]][["SGP_Configuration"]][["grade.projection.sequence"]] <- list(
+    READING=c(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12))
+SGPstateData[["WIDA_MI"]][["SGP_Configuration"]][["content_area.projection.sequence"]] <- list(
+    READING=rep("READING", 13))
+SGPstateData[["WIDA_MI"]][["SGP_Configuration"]][["max.forward.projection.sequence"]] <- list(
+    READING=7)
+
+###   Run analysis
+WIDA_MI_SGP <- abcSGP(
+        WIDA_MI_SGP,
+        years = "2022",
+        steps=c("prepareSGP", "analyzeSGP", "combineSGP"),
+        sgp.config=WIDA_MI_2022.config,
+        sgp.percentiles=FALSE,
+        sgp.projections=FALSE,
+        sgp.projections.lagged=FALSE,
+        sgp.percentiles.baseline=FALSE,
+        sgp.projections.baseline=TRUE,
+        sgp.projections.lagged.baseline=FALSE,
+        sgp.target.scale.scores=TRUE,
+        parallel.config=parallel.config
+)
+
+###   Save results
+#save(WIDA_MI_SGP, file="Data/Michigan_SGP.Rdata")
